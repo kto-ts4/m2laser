c63c40620a79cba08a298394a55ddca95b58bb83
Keisuke-Kato <keisuke-kato@g.ecc.u-tokyo.ac.jp>
2021-05-31 12:09:37 +0900

    add set_power

--- tislaser.py
+++ tislaser.py
@@ -1,8 +1,6 @@
 import socket
 
-t_id = 0
-
-def command(command, **parameters):
+def command(command, t_id, **parameters):
 	msg = ''
 	l = len(parameters)
 	if l == 0:
@@ -34,14 +32,16 @@ class Equipment:
 		init_msg = command("start_link", ip_address=myip)
 		self.s.sendall(init_msg.encode())
 		print(self.s.recv(1024))
+		self.t_id = 0
 
 class Solstis(Equipment):
 	def __init__(self, ip, port):
 		super().__init__(ip, port)
 
 	def set_wavelength(self, wl):
-		msg = command('move_wave_t',wavelength=wl)
+		msg = command('move_wave_t',t_id=self.t_id, wavelength=wl)
 		self.s.sendall(msg.encode())
+		self.t_id += 1
 		print(self.s.recv(1024))
 
 	def etalon_lock(self, operation):
@@ -50,6 +50,7 @@ class Solstis(Equipment):
 		else:
 			msg = command('etalon_lock', operation=operation)
 			self.s.sendall(msg.encode())
+			self.t_id += 1
 			print(self.s.recv(1024))
 
 class Equinox(Equipment):
@@ -62,4 +63,11 @@ class Equinox(Equipment):
 		else:
 			msg = command('laser_control', operation=operation)
 			self.s.sendall(msg.encode())
+			self.t_id += 1
 			print(self.s.recv((1024)))
+
+	def set_power(self, power):
+		msg = command('set_power', power=power)
+		self.s.sendall(msg.encode())
+		self.t_id += 1
+		print(self.s.recv(1024))
